cd39ee96616041baad370ff1daf4c7e03f60de91
Laura Berge <lauraeberge@gmail.com>
2019-08-28 00:53:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   
   while array[counter] do
-    new_array.concat(array[counter] * array[counter])
+    array[counter] *= array[counter]
     counter += 1
   end
 end
